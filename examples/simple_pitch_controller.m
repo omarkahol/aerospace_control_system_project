8979e730da34e8a9a0fff814a767afe41a7849d7
omi14098 <okahol@yahoo.it>
2021-05-27 09:13:39 +0200

    hinf synthesis

--- examples/simple_pitch_controller.m
+++ examples/simple_pitch_controller.m
@@ -17,40 +17,63 @@ b5 = ureal('b5',0.921, 'Percentage', 7);
 G = (b1*s+b2)/(b3*s^3 + b4*s^2 + b5*s);
 
 %nominal model
-G_n = getNominal(G);
+G_n = tf(getNominal(G));
 
 %MODELLO INCERTO
 G_array = usample(G,50);
 [~, Info] = ucover(G_array, G_n, 5); % CALCOLO DEL PESO DELL'INCERTEZZA
 
-%PLOT DEL PESO DELL'INCERTEZZA
-figure(1);
-hold on;
-bodemag((G_array-G_n)/G_n,'b', Info.W1,'r');
-hold off;
 
 %CONTROLLER PD
-R = tunablePID('c','pd');
-
-%NOMINAL TRANSFER FUNCTIONS
-L = R*G_n;
-S = 1/(1+L);
-F = L/(1+L);
+R = tunablePID('c','pidf');
 
 %TRANSFER FUNCTION PER IL PESO DELLA NOMINAL STABILITY
-M = 3;
-omb = 12;
-A = 1e-5;
+M = 1;
+omb = 30;
+A = 0.001;
 Wpinv = (s+omb*A)/(s/M + omb);
 Wp = 1/Wpinv;
 
-%H_INF SYNTHESIS
+%CONTROL EFFORT TRANSFER FUNCTION
+M = 1;
+omb = 30;
+A = 0.001;
+Wqinv=10*(0.001*s+1)/(100*s+1);
+Wq=1/Wqinv; 
+
+% GENSS MODEL
+R.u = 'e';
+R.y = 'u';
+Wp.u='e';
+Wp.y='ew';
+Wq.u = 'u';
+Wq.y = 'eq';
+Wpinv.u='nw';
+Wpinv.y='n';
+sum2 = sumblk('yn = y + n');
+
+G_n.u = 'u';
+G_n.y = 'y';
+sum1 = sumblk('e = y0 - yn');
+T0 = connect(Wp,Wq,Wpinv,R,G_n,sum1,sum2,{'y0', 'nw'},{'y','ew','eq'});
+
+rng(0)
 opt = hinfstructOptions('Display','final','RandomStart',20);
-K = hinfstruct([S*Wp,F*Info.W1],opt);
+T = hinfstruct(T0,opt);
+R = pid(T.Blocks.c);
 
-R = pid(K.Blocks.c); % RECOVERY DEL CONTROLLORE
+figure(1);
+bodemag(R/(1+G*R),'b-',Wqinv,'r--',{1e-5,1e+5});
 
-%SIMULAZIONE DEL MODELLO INCERTO
-L = G*R;
-F = L/(1+L);
-step(F,10);
+figure(2);
+bodemag(1/(1+G*R),'b-',Wpinv,'r--',{1e-5,1e+5});
+
+P = augw(G_n,Wp,Wq,[]);
+
+% Structured synthesis
+K0 = tunablePID('C','pid');
+opt = hinfstructOptions('Display','final','RandomStart',5);
+K = hinfstruct(P,K0,opt);
+
+figure(3),bode(1/(1+G*K),Wpinv),grid,legend('S','1/W1')
+figure(4),bode(K/(1+G*K),Wqinv),grid,legend('Q','1/W2')
--- nominal_model.m
+++ nominal_model.m
@@ -1,13 +1,64 @@
 clear variables;
 
 %NOMINAL MODEL WITH STD = 1
-[A,B,C,D] = ss_model(1, true);
+[A,B,~,~] = ss_model(3, true);
+
+C_p = [0,1,0];
+D = 0;
+G_p = tf(ss(A,B,C_p,D));
+
+s = tf('s');
+
+integrator = 1/s;
+
+% PESO SULLA SENSITIVITY FUNCTION
+M = 5;
+omb = 11;
+A = 1e-1;
+Wpinv = (s+omb*A)/(s/M + omb);
+Wp = 1/Wpinv;
+
+% PESO SULLA CONTROL EFFORT
+Wqinv = tf(1);
+Wq = 1/Wqinv;
+
+%GENSS MODEL
+Rp = tunablePID('Rp','pid');
+Rphi = tunablePID('Rphi','p');
+
+%creiamo input e output dei blocchi
+Wp.u = 'e_phi';
+Wp.y = 'e_ws';
+Wq.u = 'delta_lat';
+Wq.y = 'eq';
+Rphi.u = 'e_phi';
+Rphi.y = 'p_0';
+Rp.u = 'e_p';
+Rp.y='delta_lat';
+G_p.u='delta_lat';
+G_p.y='p';
+integrator.u='p';
+integrator.y='phi';
+
+%specify summing junctions
+sum1 = sumblk('e_phi = phi_0 - phi');
+sum2 = sumblk('e_p = p_0 - p');
+
+%create genss model
+T0 = connect(Wp,Wq,Rphi,Rp,G_p,integrator,sum1,sum2,{'phi_0'},{'phi','e_ws','eq'});
+
+rng('default')
+opt = hinfstructOptions('Display','final','RandomStart',10);
+T = hinfstruct(T0,opt);
+
+Rp = getBlockValue(T,'Rp');
+Rphi = getBlockValue(T,'Rphi');
 
 
-system = ss(A,B,C,D);
-plant = tf(system);
+L = integrator*G_p*Rp*(1- G_p*Rp/(1+G_p*Rp))*Rphi;
 
-plant
+figure(1);
+step(T,linspace(0,0.1,1000));
 
-bode(plant);
-
+figure(2);
+bodemag(1/(1+L), 'b-', Wpinv, 'r--');
