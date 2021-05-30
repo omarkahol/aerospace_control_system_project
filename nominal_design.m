52688e94ea1a1190a08f6723e1246edcd38865f6
omi14098 <okahol@yahoo.it>
2021-05-30 17:53:04 +0200

    final hinf and verification

--- nominal_design.m
+++ nominal_design.m
@@ -6,22 +6,26 @@ addpath('lib');
 
 % PERFORMANCE REQUIREMENT
 s = tf('s');
-wn = 12;
-csi = 0.96;
+wn = 15;
+csi = 0.98;
 L_so = 1/(1+2*csi*(s/wn) + (s/wn)^2);
 Wpinv = 1/(1+L_so);
 Wp = 1/Wpinv;
 
+% CONTROL REQUIRMENT
+epsu = 0.01;
+wbu = 1;
+Mu = 2;
+Wqinv = (epsu * s + wbu)/(s+wbu/Mu);
+Wq = 1/Wqinv;
 %--------------------------------------------------------------------------
 %GENSS MODEL
 Rp = tunablePID('Rp','pid');
 Rphi = tunablePID('Rphi','p');
+
 integrator = 1/s;
 
-Wqinv = tf(1);
-Wq = 1/Wqinv;
 %creiamo input e output dei blocchi
-
 Rphi.u = 'ephi';
 Rphi.y = 'p_0';
 Rp.u = 'e_p';
@@ -36,7 +40,7 @@ sum1 = sumblk('ephi = \phi_0 - \phi');
 sum2 = sumblk('e_p = p_0 - p');
 
 %create genss model
-T0 = minreal(connect(Rphi,Rp,G,integrator,sum1,sum2,{'\phi_0'},{'ephi','\delta_{lat}'}));
+T0 = minreal(connect(Rphi,Rp,G,integrator,sum1,sum2,{'\phi_0'},{'ephi','\delta_{lat}','\phi'}));
 
 rng('default');
 
@@ -48,7 +52,52 @@ Req = [
 
 opt = systuneOptions('RandomStart',N_TESTS, 'SoftTol', 1e-7, 'Display', 'iter');
 [T, J, ~] = systune(T0,Req, opt);
+
 figure(1);
-step(T);
+t1=1; t2 = 2; 
+step(T*10*(1-2*exp(-t1*s) + exp(-s*t2)));
+
+
+[A,B,C,D,~] = ss_model(3, false);
+G = uss(A,B,C,D);
+
+%GENSS MODEL
+Rp = T.blocks.Rp;
+Rphi = T.blocks.Rphi;
+
+integrator = 1/s;
+
+%creiamo input e output dei blocchi
+Rphi.u = 'ephi';
+Rphi.y = 'p_0';
+Rp.u = 'e_p';
+Rp.y='\delta_{lat}';
+G.u='\delta_{lat}';
+G.y='p';
+integrator.u='p';
+integrator.y='\phi';
+
+%specify summing junctions
+sum1 = sumblk('ephi = \phi_0 - \phi');
+sum2 = sumblk('e_p = p_0 - p');
+
+%create genss model
+T = minreal(connect(Rphi,Rp,G,integrator,sum1,sum2,{'\phi_0'},{'ephi','\delta_{lat}','\phi'}));
+Q = getIOTransfer(T, '\phi_0','\delta_{lat}');
+S = getIOTransfer(T, '\phi_0','ephi');
+
+figure(2);
+bodemag(S,'b-',Wpinv,'r-');
+
+figure(3);
+bodemag(Q,'b-',Wqinv,'r-');
+
+figure(4);
+step(T*10*(1-2*exp(-t1*s) + exp(-s*t2)));
+
+
+
+
+
 
 
---
+++ robustness_verification.m
@@ -0,0 +1,1 @@
+run('nominal_design.m');
