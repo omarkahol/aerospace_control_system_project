9f7a36d21ec6e3e1a55a21430da8bdf7593462a5
omi14098 <okahol@yahoo.it>
2021-05-30 18:13:00 +0200

    commented code

--- nominal_design.m
+++ nominal_design.m
@@ -2,72 +2,89 @@ clear variables;
 addpath('lib');
 
 %NOMINAL MODEL WITH STD = 1
-[~,~,~,~,G] = ss_model(3, true);
+%creo il modello nominale (quindi passando true a ss_model)
+[~,~,~,~,G] = ss_model(1, true);
 
 % PERFORMANCE REQUIREMENT
-s = tf('s');
-wn = 15;
-csi = 0.98;
-L_so = 1/(1+2*csi*(s/wn) + (s/wn)^2);
-Wpinv = 1/(1+L_so);
+s = tf('s'); %variabile di laplace s
+wn = 15; %crossover frequency
+csi = 0.98; %damping ration
+L_so = 1/(1+2*csi*(s/wn) + (s/wn)^2); %open loop transfer function --> obiettivo
+%pesi
+Wpinv = 1/(1+L_so); 
 Wp = 1/Wpinv;
 
 % CONTROL REQUIRMENT
-epsu = 0.01;
-wbu = 1;
-Mu = 2;
+epsu = 0.01; %valore a frequenze elevate
+wbu = 1; % crossover frequency
+Mu = 2; %valore a frequenze basse 
+%psei
 Wqinv = (epsu * s + wbu)/(s+wbu/Mu);
 Wq = 1/Wqinv;
+
 %--------------------------------------------------------------------------
 %GENSS MODEL
-Rp = tunablePID('Rp','pid');
-Rphi = tunablePID('Rphi','p');
+%--------------------------------------------------------------------------
+Rp = tunablePID('Rp','pid'); %controller PID
+Rphi = tunablePID('Rphi','p'); %controller P
 
+%blocco integratore
 integrator = 1/s;
 
 %creiamo input e output dei blocchi
-Rphi.u = 'ephi';
+
+%controller phi
+Rphi.u = 'ephi'; 
 Rphi.y = 'p_0';
+
+%controller p
 Rp.u = 'e_p';
 Rp.y='\delta_{lat}';
+
+%plant
 G.u='\delta_{lat}';
 G.y='p';
+
+%uscita
 integrator.u='p';
 integrator.y='\phi';
 
-%specify summing junctions
+%blocchi somma
 sum1 = sumblk('ephi = \phi_0 - \phi');
 sum2 = sumblk('e_p = p_0 - p');
 
-%create genss model
+%modello gens
 T0 = minreal(connect(Rphi,Rp,G,integrator,sum1,sum2,{'\phi_0'},{'ephi','\delta_{lat}','\phi'}));
 
-rng('default');
+%--------------------------------------------------------------------------
+% H INFINITY SYNTHESIS
+%--------------------------------------------------------------------------
+% LANCIAMO N_TEST CICLI DI OTTIMIZZAZIONE 
 
-N_TESTS = 10;
-Req = [
+rng('default'); %inizializza il random number generator
+
+N_TESTS = 10; %numero di dati iniziali random
+%requisiti
+Req = [ 
     TuningGoal.WeightedGain('\phi_0','ephi',Wp, 1);
     TuningGoal.WeightedGain('\phi_0','\delta_{lat}',Wq, 1)
 ];
 
 opt = systuneOptions('RandomStart',N_TESTS, 'SoftTol', 1e-7, 'Display', 'iter');
-[T, J, ~] = systune(T0,Req, opt);
-
-figure(1);
-t1=1; t2 = 2; 
-step(T*10*(1-2*exp(-t1*s) + exp(-s*t2)));
+[T, J, ~] = systune(T0,Req, opt); 
 
 
 [A,B,C,D,~] = ss_model(3, false);
 G = uss(A,B,C,D);
 
-%GENSS MODEL
-Rp = T.blocks.Rp;
-Rphi = T.blocks.Rphi;
+%--------------------------------------------------------------------------
+% MODELLO INCERTO
+%-------------------------------------------------------------------------- 
 
+%RIDEFINISCO I BLOCCHI
+Rp = T.blocks.Rp; %recupero i valori di Rp
+Rphi = T.blocks.Rphi; %recupero i valori di Rphi
 integrator = 1/s;
-
-%creiamo input e output dei blocchi
 Rphi.u = 'ephi';
 Rphi.y = 'p_0';
 Rp.u = 'e_p';
@@ -76,23 +93,25 @@ G.u='\delta_{lat}';
 G.y='p';
 integrator.u='p';
 integrator.y='\phi';
-
-%specify summing junctions
 sum1 = sumblk('ephi = \phi_0 - \phi');
 sum2 = sumblk('e_p = p_0 - p');
 
-%create genss model
+% MODELLO GENSS INCERTO
 T = minreal(connect(Rphi,Rp,G,integrator,sum1,sum2,{'\phi_0'},{'ephi','\delta_{lat}','\phi'}));
-Q = getIOTransfer(T, '\phi_0','\delta_{lat}');
-S = getIOTransfer(T, '\phi_0','ephi');
+Q = getIOTransfer(T, '\phi_0','\delta_{lat}'); % SENSITIVITY FUNCTION
+S = getIOTransfer(T, '\phi_0','ephi'); %CONTROL EFFORT TRANSFER FUNCTION
 
-figure(2);
+% PLOT DELLA SENSITIVITY FUNCTION
+figure(1);
 bodemag(S,'b-',Wpinv,'r-');
 
-figure(3);
+% PLOT DELLA CONTROL EFFORT
+figure(2);
 bodemag(Q,'b-',Wqinv,'r-');
 
-figure(4);
+% RISPOSTA AL DOPPIO STEP
+figure(3);
+t1 = 1; t2 = 2;
 step(T*10*(1-2*exp(-t1*s) + exp(-s*t2)));
 
 
--- robustness_verification.m
+++
@@ -1,1 +0,0 @@
-run('nominal_design.m');
